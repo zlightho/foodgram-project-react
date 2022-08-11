c6018ac8d6f6be1990c934cc6152f24bc24250fa
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-11 20:59:04 +0300

    tests actions

--- backend/tags/urls.py
+++ backend/tags/urls.py
@@ -1,10 +1,7 @@
-from django.db import router
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .models import Tag
 from .views import TagViewSet
-
 
 router = DefaultRouter()
 router.register(r"^tags", TagViewSet)
