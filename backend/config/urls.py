7946c71c41236a3a14eab713e4ba01e85a90480c
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-12 19:55:24 +0300

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("api/", include("djoser.urls")),
     path("api/", include("tags.urls")),
     path("api/", include("users.urls")),
+    path("api/", include("food.urls")),
 ]
---
+++ backend/food/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import IngredientViewSet
+
+router = DefaultRouter()
+router.register(r"^ingredients", IngredientViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
