7f43140baa3e9ba197c137ec4857d4be74046a8f
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-12 20:16:12 +0300

    management commands

---
+++ backend/food/management/commands/create_ingredients.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from food.models import Ingredient
+
+FILES = os.path.join(settings.BASE_DIR), "data/ingredients.json"
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open(FILES) as file:
+            data = json.load(file)
+        for ingredient in data:
+            Ingredient.objects.get_or_create(**ingredient)
