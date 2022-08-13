0bce82a4991597f6da64b0f7ad6012b1e80202ec
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-13 18:45:27 +0300

    utils

--- backend/food/utils.py
+++ backend/food/utils.py
@@ -1,4 +1,3 @@
-from requests import delete
 from .models import Recipe
 from rest_framework.response import Response
 from .serializers import RecipeShortSerializer
@@ -8,7 +7,7 @@ def helper_model_create_delete(request, model, pk):
     if pk is None or not Recipe.objects.filter(id=pk).exists():
         return Response("Отсутствует данный рецепт", status=404)
     recipe = Recipe.objects.get(id=pk)
-    if request.method.lower() == delete():
+    if request.method.lower() == "delete":
         model.objects.get(user=request.user, recipe=recipe)
         return Response(status=204)
     model.object.get_or_create(user=request.user, recipe=recipe)
