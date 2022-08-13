97097ffb839b7740e33115628cdd7a0c2e5dba18
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-13 18:06:12 +0300

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("djoser.urls.authtoken")),
-    path("api/", include("djoser.urls")),
     path("api/", include("tags.urls")),
     path("api/", include("users.urls")),
     path("api/", include("food.urls")),
+    path("api/auth/", include("djoser.urls.authtoken")),
+    path("api/", include("djoser.urls")),
 ]
