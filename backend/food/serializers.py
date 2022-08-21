81fee07bb7d651443f09815fee48a006b8d7f63f
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2022-08-21 13:07:49 +0300

    serializers

--- backend/food/serializers.py
+++ backend/food/serializers.py
@@ -1,5 +1,4 @@
 from django.db.transaction import atomic
-from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
